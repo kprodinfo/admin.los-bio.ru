eb0cde89bff729cfded058874790c3692fe12587
Andrey Kolesov <kolesov@kprod.agency>
2024-05-24 23:06:32 +0700

    Fix new product id

--- components/catalog/product-form/product-form.vue
+++ components/catalog/product-form/product-form.vue
@@ -136,6 +136,7 @@ function removeFile($file) {
 async function sendProduct() {
   /* Если создаём новый продукт */
   if (slug === "create") {
+    delete product.value["id"];
     const newProduct = await createProduct({...product.value, photos: photos.value.filter(p => !p.isDeleted) });
     return await router.push({
       path: `/catalog/${newProduct.slug}`,
